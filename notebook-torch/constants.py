a88b65133f693a7afb89ccbc5711b65e641bf741
hedredo <lekeuj@hotmail.com>
2025-04-27 19:39:59 +0200

    Updating the training, val and test worklow, loading the constants.py, prepare the code for every experiments

---
+++ notebook-torch/constants.py
@@ -0,0 +1,11 @@
+"""Constants for the notebook-torch project
+"""
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+ROOT_FOLDER = Path(__file__).parent.parent.resolve()
+SEED = 314
+SAMPLING = 32
+TEST_SIZE = 0.15
+VAL_SIZE = 0.15
+BATCH_SIZE = 8
