ec3c93b566d52ae5e219988fe3f432f9f584899d
hedredo <lekeuj@hotmail.com>
2025-05-03 00:12:52 +0200

    update a bit of code on tf-notebook

---
+++ notebook-tf/constants.py
@@ -0,0 +1,18 @@
+"""Constants for the notebook-torch project
+"""
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+# FOLDERS VARS
+ROOT_FOLDER = Path(__file__).parent.parent.resolve()
+IMAGE_FOLDER = ROOT_FOLDER / "data" / "images"
+ARTIFACTS_FOLDER = ROOT_FOLDER / "artifacts"
+DATASET_PATH = ROOT_FOLDER / "data" / "dataset_cleaned.pkl"
+
+# TRAINING VARS
+SEED = 314
+SAMPLING = 32
+TEST_SIZE = 0.15
+VAL_SIZE = 0.15
+BATCH_SIZE = 8
+INPUT_RESOLUTION = (224, 224, 3)
