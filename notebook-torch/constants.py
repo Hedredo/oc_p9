be62ddcc64def7047fe4aeaf9471d9d3d7cdac7d
hedredo <lekeuj@hotmail.com>
2025-05-02 19:58:18 +0200

    Update the class pipeline, artifacts & metrics with tensor, adding the titles & cleaning

--- notebook-torch/constants.py
+++ notebook-torch/constants.py
@@ -3,15 +3,21 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
+# FOLDERS VARS
 ROOT_FOLDER = Path(__file__).parent.parent.resolve()
+IMAGE_FOLDER = ROOT_FOLDER / "data" / "images"
+ARTIFACTS_FOLDER = ROOT_FOLDER / "artifacts"
+DATASET_PATH = ROOT_FOLDER / "data" / "dataset_cleaned.pkl"
+
+# TRAINING VARS
 SEED = 314
-SAMPLING = 128
+SAMPLING = 32
 TEST_SIZE = 0.15
 VAL_SIZE = 0.15
 BATCH_SIZE = 8
 INPUT_RESOLUTION = (3, 224, 224)
 
-# Mamba hidden sizes depending of the model card
+# MAMBAVISION DICT {"HUGGINFACE Model Card": feature_extractor_size}
 MAMBA_HIDDEN_SIZES = {
     "nvidia/MambaVision-T-1K": 640,
     "nvidia/MambaVision-T2-1K": 640,
