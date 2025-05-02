ac7e0c83ef74593c40d3d6e5c0870b31e16b8c94
hedredo <lekeuj@hotmail.com>
2025-05-02 12:48:41 +0200

    update the TODO and the workflow, functions, classes, etc...

--- notebook-torch/constants.py
+++ notebook-torch/constants.py
@@ -9,3 +9,15 @@ SAMPLING = 128
 TEST_SIZE = 0.15
 VAL_SIZE = 0.15
 BATCH_SIZE = 8
+INPUT_RESOLUTION = (3, 224, 224)
+
+# Mamba hidden sizes depending of the model card
+MAMBA_HIDDEN_SIZES = {
+    "nvidia/MambaVision-T-1K": 640,
+    "nvidia/MambaVision-T2-1K": 640,
+    "nvidia/MambaVision-S-1K": 768,
+    "nvidia/MambaVision-B-1K": 1024,
+    "nvidia/MambaVision-L-1K": 1568,
+    "nvidia/MambaVision-B-21K": 1024,
+    "nvidia/MambaVision-L-21K": 1568,
+}
